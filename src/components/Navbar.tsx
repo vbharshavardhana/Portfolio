1c41519032567b98ca81159fbd8738c03180bf2c
V B Harshavardhana <harshavardhanavb@gmail.com>
2025-01-05 20:16:24 +0530

    Updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,28 @@
-import { Home, Briefcase, BookOpen } from 'lucide-react';
+import { Home, Briefcase, BookOpen, Menu } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="bg-[#1D2528] py-4">
       <div className="container mx-auto px-4 flex justify-between items-center">
-        <Link to="/" className="text-xl font-semibold">
+        <Link to="/" className="text-xl font-semibold text-white">
           vbharshavardhana
         </Link>
-        <div className="flex gap-6">
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="block md:hidden text-white"
+          aria-label="Toggle Menu"
+        >
+          <Menu size={24} />
+        </button>
+        <div
+          className={`${
+            menuOpen ? 'block' : 'hidden'
+          } md:flex gap-6 items-center text-white`}
+        >
           <Link to="/" className="flex items-center gap-2 hover:text-gray-300">
             <Home size={18} />
             Home
